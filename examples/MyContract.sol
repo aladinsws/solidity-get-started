09e5e72def5b81dff3164d06679ff1e4b7ded06f
alatsuki <aladinsws@gmail.com>
2024-05-12 17:51:55 +0200

    array type examples

--- examples/MyContract.sol
+++ examples/MyContract.sol
@@ -2,27 +2,17 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    // State Variables
-    int256 public myInt = 1;
-    uint public myUint  = 1;
-    uint256 public myUint256 = 1;
-    uint8 public myUint8 = 1;
+    // Arrays 
+    uint [] public uintArray = [1,2,3];
+    string[] public stringArray =["apple","banana","carrot"];
+    string[] public values;
+    uint256 [][] public array2D = [[1,2,3],[4,5,6]];
 
-    string public myString = "Hello world !";
-    bytes32 public myBytes32 = "Hello world !";
-
-    address public myAdress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
-    struct Mystruct {
-        uint256 myUinit256;
-        string myString;
+    function addValue(string memory _value) public {
+        values.push(_value);
     }
-    
-    Mystruct public mystruct = Mystruct(1,"Hello , world!");
 
-    // Local Variables
-    function getValue() public pure returns(uint){
-        uint value = 1;
-        return value;
+    function valueCount() public view returns(uint){
+        return values.length;
     }
 }
